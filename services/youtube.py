f0ac2ec2ca50c8b2fd89fd81a6fb11ba70233da7
seyedali-rafazi <seyedalirafazi80@gmail.com>
2026-04-24 02:46:12 -0700

    feat: add cookie for download just

--- services/youtube.py
+++ services/youtube.py
@@ -104,6 +104,7 @@ def download_youtube_video(url, progress_dict=None):
 
     ydl_opts = {
         "proxy": PROXY,
+        "cookiefile": "www.youtube.com_cookies.txt",
         # استفاده از فرمت کد قدیمی که بدون مشکل کار می‌کرد
         "format": "best[height<=720][filesize<300M]/best[height<=480][filesize<300M]/best[height<=360]/worst",
         "outtmpl": os.path.join(DOWNLOAD_DIR, f"%(id)s_{req_id}.%(ext)s"),
@@ -151,6 +152,7 @@ def download_youtube_audio(url, progress_dict=None):
 
     ydl_opts = {
         "proxy": PROXY,
+        "cookiefile": "www.youtube.com_cookies.txt",
         "format": "bestaudio/best",
         "postprocessors": [
             {
