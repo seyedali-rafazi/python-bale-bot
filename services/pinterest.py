00be8d1ece7c6d256b875bfd9ec5ebe0690f51f3
seyedali-rafazi <seyedalirafazi80@gmail.com>
2026-05-02 09:07:29 -0700

    fix: get pinterst photo

--- services/pinterest.py
+++ services/pinterest.py
@@ -4,7 +4,9 @@ from ddgs import DDGS
 def search_pinterest_images(query, max_results=10):
     try:
         with DDGS() as ddgs:
-            results = list(ddgs.images(query, max_results=max_results))
+            # اضافه کردن site:pinterest.com به کوئری تا فقط در پینترست بگردد
+            pinterest_query = f"{query} site:pinterest.com"
+            results = list(ddgs.images(pinterest_query, max_results=max_results))
             return [res["image"] for res in results]
     except Exception as e:
         print(f"Pinterest Search Error: {e}")
