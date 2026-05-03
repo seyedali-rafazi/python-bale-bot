29150e27fc8c192e646d9007f32fcd11b6172ff3
seyedali-rafazi <seyedalirafazi80@gmail.com>
2026-05-03 06:36:04 -0700

    to dev

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 
+
 import logging
 from telegram.ext import ApplicationBuilder, ContextTypes
 from handlers import register_all_handlers
@@ -61,22 +62,12 @@ def main():
     # ثبت تمام هندلرها از پوشه handlers
     register_all_handlers(application)
 
-    print("✅ ربات با معماری جدید و وب‌هوک با موفقیت راه‌اندازی شد...")
-
-    # ---------------------------------------------------------
-    # تنظیمات مربوط به Webhook
-    # ---------------------------------------------------------
-    # پورتی که Nginx ترافیک را به آن می‌فرستد (در تنظیمات Nginx عدد 8443 را وارد کرده بودیم)
-    PORT = int(os.environ.get("PORT", "8443"))
-
-    # آدرس دقیق وب‌هوک همراه با توکن برای امنیت بیشتر
-    WEBHOOK_URL = f"https://bale.iranisafar.lol/{BALE_TOKEN}"
-
-    # راه‌اندازی ربات در حالت وب‌هوک
-    application.run_webhook(
-        listen="0.0.0.0", port=PORT, url_path=BALE_TOKEN, webhook_url=WEBHOOK_URL
-    )
+    print("✅ ربات با معماری جدید با موفقیت راه‌اندازی شد...")
+    application.run_polling()
 
 
 if __name__ == "__main__":
     main()
+
+
+
