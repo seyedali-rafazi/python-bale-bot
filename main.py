fefd317babeff6511361f9ebe21f469a9e28318c
seyedali-rafazi <seyedalirafazi80@gmail.com>
2026-05-05 15:03:27 -0700

    change to webhook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # main.py
 
-
 import logging
 from telegram.ext import ApplicationBuilder, ContextTypes
 from handlers import register_all_handlers
@@ -11,6 +10,9 @@ from core.database import init_db
 
 load_dotenv()
 BALE_TOKEN = os.getenv("BALE_TOKEN")
+BALE_URL = os.getenv("BALE_URL")
+BALE_LISTENING_PORT = os.getenv("BALE_LISTENING_PORT")
+
 
 # تنظیمات لاگ‌گیری
 logging.basicConfig(
@@ -40,10 +42,11 @@ async def cleanup_old_downloads(context: ContextTypes.DEFAULT_TYPE):
                         pass
 
 
-async def main_async():
+def main():
     # ساخت جداول دیتابیس در هنگام استارت شدن بات
     init_db()
 
+    # ساخت اپلیکیشن با پشتیبانی از job_queue
     application = (
         ApplicationBuilder()
         .token(BALE_TOKEN)
@@ -52,25 +55,30 @@ async def main_async():
         .build()
     )
 
-    # پاک کردن آپدیت‌های قدیمی قبل از شروع polling
-    await application.bot.delete_webhook(drop_pending_updates=True)
-
-    # job_queue
+    # افزودن تسک پاکسازی پوشه (هر ۷۲۰۰ ثانیه یک بار اجرا می‌شود، اولین اجرا ۱۰ ثانیه بعد از استارت)
     if application.job_queue:
         application.job_queue.run_repeating(
             cleanup_old_downloads, interval=7200, first=10
         )
 
+    # ثبت تمام هندلرها از پوشه handlers
     register_all_handlers(application)
 
-    print("✅ ربات با معماری جدید با موفقیت راه‌اندازی شد...")
-    await application.run_polling()
+    print("✅ ربات با معماری جدید و وب‌هوک با موفقیت راه‌اندازی شد...")
 
+    # ---------------------------------------------------------
+    # تنظیمات مربوط به Webhook
+    # ---------------------------------------------------------
+    # پورتی که Nginx ترافیک را به آن می‌فرستد (در تنظیمات Nginx عدد 8443 را وارد کرده بودیم)
+    PORT = int(os.environ.get("PORT", BALE_LISTENING_PORT))
 
-def main():
-    import asyncio
+    # آدرس دقیق وب‌هوک همراه با توکن برای امنیت بیشتر
+    WEBHOOK_URL = f"{BALE_URL}/{BALE_TOKEN}"
 
-    asyncio.run(main_async())
+    # راه‌اندازی ربات در حالت وب‌هوک
+    application.run_webhook(
+        listen="0.0.0.0", port=PORT, url_path=BALE_TOKEN, webhook_url=WEBHOOK_URL
+    )
 
 
 if __name__ == "__main__":
