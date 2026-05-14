b25858910fbfa6bba98be5c8f5a5dc08c6e5b381
seyedali-rafazi <seyedalirafazi80@gmail.com>
2026-05-14 08:04:24 -0700

    fix: download web client

--- services/youtube.py
+++ services/youtube.py
@@ -64,7 +64,7 @@ def _base_ytdlp_cmd():
 
     cmd = [
         "yt-dlp",
-        "--force-ipv6",
+        "--force-ipv4",
         "--source-address",
         random_ip,
         "--js-runtimes",
@@ -72,7 +72,7 @@ def _base_ytdlp_cmd():
         "--remote-components",
         "ejs:github",
         "--extractor-args",
-        "youtube:player_client=web",
+        "youtube:player_client=android,web,ios",
         "--no-playlist",
     ]
 
